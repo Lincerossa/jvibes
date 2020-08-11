6b945161314a4292beff400bdbeec4a620b1efbf
marcello luatti <marcello.luatti@e-globe-solutions.com>
2020-08-11 18:14:46 +0200

    feat: 🚀 basic play/stop audioContext implementation.  This is a customHook that will be compiled by rollup and right now published throught yalc, that simulates the npm publishing procedure :D

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React, { useEffect } from "react"
+
+import React, { useState, useEffect, useCallback } from 'react'
+
+import Microphone from './utils/Microphone'
+import Player from './utils/Player'
 
 
-export { default as useRecord } from "./hooks/useRecord"
+export default (status) => {
 
-export default () => {
+  const [ tracks, setTracks ] = useState([])
+  const [ isRecording, setIsRecording] = useState(null)
+  const [ MicrophoneIstance, setMicrophoneIstance ] = useState(null)
+
+  const onStart = () => setIsRecording(true)
+
+  const onStop = useCallback((track) => {
+    setTracks([...tracks, track])
+    setIsRecording(false)
+  }, [tracks, setTracks, setIsRecording])
+
+  useEffect(()=> {
+    const MicrophoneIstance = new Microphone(onStart, onStop)
+    setMicrophoneIstance(MicrophoneIstance)
+  }, [onStop])
+
+
   useEffect(() => {
-    console.warn("🚀Mounted")
-  }, []);
+    if(!MicrophoneIstance) return 
+    if(status === "start") MicrophoneIstance.startRecording()
+    if(status === "stop") MicrophoneIstance.stopRecording()
+  }, [status, MicrophoneIstance])
 
-  return <div>Check in console. There should be a warning</div>
+
+  return {
+    tracks,
+    isRecording
+  }
 }
